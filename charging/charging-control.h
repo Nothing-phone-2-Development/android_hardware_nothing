663a6ec133aad2acee438100ba10ad673fcdf579
Abhay Singh Gill <abhaygill017@gmail.com>
2024-09-05 23:36:46 +0530

    Add Nothing charging control service
    Signed-off-by: Abhay Singh Gill <abhaygill017@gmail.com>

---
+++ charging/charging-control.h
@@ -0,0 +1,34 @@
+/*
+ * Copyright (C) 2024 The LineageOS Project
+ * Copyright (C) 2024 Neoteric OS
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#pragma once
+
+#include <aidl/android/hardware/health/IHealth.h>
+#include <aidl/android/hardware/health/IHealthInfoCallback.h>
+#include <aidl/android/hardware/health/BnHealthInfoCallback.h>
+#include <string.h>
+
+using aidl::android::hardware::health::BatteryStatus;
+using aidl::android::hardware::health::HealthInfo;
+using aidl::android::hardware::health::IHealthInfoCallback;
+using aidl::android::hardware::health::IHealth;
+using aidl::android::hardware::health::BnHealthInfoCallback;
+
+using namespace std;
+
+class ChargeStatusListener : public BnHealthInfoCallback {
+public:
+    BatteryStatus mStatus;
+    bool usbChgActive;
+    bool usingPps;
+    bool wlsChgActive;
+    bool enableChgLimit;
+    ndk::ScopedAStatus healthInfoChanged(const HealthInfo& info) override;
+    void limitCharge();
+    int write_value(const char *file, const char *value);
+    int write_value(const char *file, int value);
+    string read_line(const char *file);
+};
