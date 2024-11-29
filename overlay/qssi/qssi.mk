90dd9fc3457ed06d9cd7ba52f4a301d99ab9f6f7
chandu078 <chandudyavanapelli03@gmail.com>
2024-11-29 16:10:49 +0530

    Import cleaned QSSI RROs from NOS 2.5.1
    Signed-off-by: chandu078 <chandudyavanapelli03@gmail.com>

---
+++ overlay/qssi/qssi.mk
@@ -0,0 +1,16 @@
+#
+# Copyright (C) 2023 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_PACKAGES += \
+    CarrierConfigResCommon \
+    FrameworksResCommon \
+    NTWifiResCommon \
+    SystemUIResCommon \
+    TelephonyResCommon \
+    WifiResCommon
+
+PRODUCT_SOONG_NAMESPACES += \
+    $(LOCAL_PATH)
