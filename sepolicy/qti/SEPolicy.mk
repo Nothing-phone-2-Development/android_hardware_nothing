67b136c1666095877f257b48b536507c5d0edc87
chandu078 <chandudyavanapelli03@gmail.com>
2024-11-29 18:28:00 +0530

    Add initial QTI SEPolicy for Nothing
    Signed-off-by: chandu078 <chandudyavanapelli03@gmail.com>

---
+++ sepolicy/qti/SEPolicy.mk
@@ -0,0 +1,8 @@
+#
+# Copyright (C) 2023 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+BOARD_VENDOR_SEPOLICY_DIRS += hardware/nothing/sepolicy/qti/vendor
+SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += hardware/nothing/sepolicy/qti/private
